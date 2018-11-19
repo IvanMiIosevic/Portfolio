fa4dbe5ff7780a58721cc247477168b1f66499d0
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2018-11-19 22:34:05 +0100

    Add typescript support

--- typings/vue-shim.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.vue" { // declare vue module so that typescript can find vue files
-    import Vue from "vue";
-    export default Vue;
-}
-declare module '@fortawesome/vue-fontawesome';
