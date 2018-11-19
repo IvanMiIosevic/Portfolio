eca3fb0dea756788cbaea083eef7e09e66ddb7e7
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2018-11-19 22:34:35 +0100

    Add contact icons

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,6 +5,11 @@
         <h1 class="title">
           Contact:
         </h1>
+        <a class="contact-link" href="mailto:contact.ivanmilosevic@gmail.com"><font-awesome-icon class="contact-icon" icon="envelope" /></a>
+        <a class="contact-link" href="https://github.com/Alvai"><font-awesome-icon class="contact-icon" :icon="['fab', 'github']" /></a>
+        <a class="contact-link" href="https://twitter.com/Alvai__"><font-awesome-icon class="contact-icon" :icon="['fab', 'twitter']" /></a>
+        <a class="contact-link" href="https://stackoverflow.com/users/9940385/ivan-milosevic"><font-awesome-icon class="contact-icon" :icon="['fab', 'stack-overflow']" /></a>
+        <a class="contact-link" href="https://www.linkedin.com/in/ivan-milosevic"><font-awesome-icon class="contact-icon" :icon="['fab', 'linkedin']" /></a>
       </div>
     </div>
   </section>
@@ -14,10 +19,32 @@
 .contact {
   background-color: #fdfffc;
 }
+svg.svg-inline--fa.contact-icon {
+  font-size: 50px;
+  margin: 5px;
+  color: #ffd779;
+  :hover {
+    color: #333c3e;
+  }
+}
 </style>
 
 <script lang="ts">
 import Vue from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faTwitter,
+  faStackOverflow,
+  faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faLinkedin, faGithub, faEnvelope, faTwitter, faStackOverflow);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 export default Vue.extend({
   name: "Contact"
 });
