94d08553b081e6d3b3eda6b871510b85c03b0fcd
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2018-12-25 23:10:57 +0100

    switch color to variable

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $secondary-color;
 }
 .is-marged-right {
   margin-right: 0.5rem;
