7af6b3fa30977c7798c89b7f740e2b7f6925ee74
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2018-12-25 23:15:37 +0100

    Add projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -37,7 +37,7 @@
   margin: 0 auto;
 }
 .projects {
-  background-color: #ffda6b;
+  background-color: $primary-color;
   .card {
     border-radius: 25px;
     box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
@@ -66,6 +66,7 @@ import Resume from "@/components/Resume.vue";
 export default Vue.extend({
   data() {
     return {
+      // TO DO: Move projects to an api
       projects: [
         {
           title: "Pokedex",
@@ -74,7 +75,7 @@ export default Vue.extend({
           image_URL: "https://ouch.alvai.io/pokedex.png"
         },
         {
-          title: "Stunning octo goggles",
+          title: "Stunning octo goggles (WIP)",
           description: "Express API avec GraphQL && Typescript",
           project_URL: "https://github.com/Alvai/stunning-octo-goggles",
           image_URL: "https://ouch.alvai.io/graphql.png"
@@ -86,10 +87,24 @@ export default Vue.extend({
           image_URL: "https://ouch.alvai.io/homepilot_logo.png"
         },
         {
-          title: "Snippy Snippets",
+          title: "Snippy Snippets (WIP)",
           description: "Site de partage de bout de code fait en Vue JS",
           project_URL: "https://github.com/Alvai/snippy-snippets",
           image_URL: "https://ouch.alvai.io/vue_white.jpeg"
+        },
+        {
+          title: "Bethesleak",
+          description:
+            "Site de gestion de ticket pour Bethesleak (Privée jusqu'au rendu)",
+          project_URL: "https://ouch.alvai.io/info/comingSoon",
+          image_URL: "https://ouch.alvai.io/Bethesleak.png"
+        },
+        {
+          title: "too-short",
+          description: "raccourcisseur d'url en Node.js",
+          project_URL: "https://github.com/Alvai/too-short",
+          image_URL:
+            "https://mybigplunge.com/wp-content/uploads/2018/04/url-shortener-theplungedaily.jpg"
         }
       ]
     };
