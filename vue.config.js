fbd57a6490da64b7ab817ee33966913eb336a02e
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2018-12-25 23:10:38 +0100

    Add css variables

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+            @import "@/../node_modules/bulma/sass/utilities/initial-variables.sass";
+            @import "@/assets/styles/_variables.scss";
+        `
+      }
+    }
+  }
+}
